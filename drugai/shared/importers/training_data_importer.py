a9ad2c681cc978447aad34dffde2fdebdeb8af00
daiyizheng <387942239@qq.com>
2022-07-27 21:11:31 +0800

    import

--- drugai/shared/importers/training_data_importer.py
+++ drugai/shared/importers/training_data_importer.py
@@ -6,10 +6,14 @@
 # @File    : training_data_importer.py
 from __future__ import annotations, print_function
 
-from abc import ABC, abstractmethod
 from typing import Optional, Text, Union, List, Any, Dict
+import logging
+from abc import ABC, abstractmethod
 
+from drugai.shared.importers.drug_importer import DrugImporter
 from drugai.shared.training_data import TrainingData
+import drugai.utils.common
+from drugai.utils.io import read_config_yaml
 
 
 class TrainingDataImporter(ABC):
@@ -42,3 +46,50 @@ class TrainingDataImporter(ABC):
             Loaded data `TrainingData`.
         """
         ...
+    
+    @staticmethod
+    def load_from_config(config_file: Optional[Text] = None,
+                         train_data_paths: Optional[Union[List[Text], Text]] = None,
+                         eval_data_paths: Optional[Union[List[Text], Text]] = None,
+                         test_data_paths: Optional[Union[List[Text], Text]] = None):
+        config = read_config_yaml(config_file) ## 加载config yml文件
+        return TrainingDataImporter.load_from_dict(config=config,
+                                                  config_file=config_file,
+                                                  train_data_paths=train_data_paths,
+                                                  eval_data_paths=eval_data_paths,
+                                                  test_data_paths=test_data_paths)
+
+    @staticmethod
+    def load_from_dict(config: Optional[Dict] = None,
+                       config_file: Optional[Text] = None,
+                       train_data_paths: Optional[Union[List[Text], Text]] = None,
+                       eval_data_paths: Optional[Union[List[Text], Text]] = None,
+                       test_data_paths: Optional[Union[List[Text], Text]] = None):
+        from drugai.shared.importers.drug_importer import DrugImporter
+        config = config or {}
+        importers = config.get("importers", [])  ## 导入依赖,这里是自定义的导入模块
+        importers = [
+            TrainingDataImporter._importer_from_dict(
+                importer, config_file, train_data_paths, eval_data_paths, test_data_paths
+            )
+            for importer in importers
+        ]
+
+    @staticmethod
+    def _importer_from_dict(importer_config: Dict,
+                            config_path: Text,
+                            train_data_paths: Text, 
+                            eval_data_paths: Text, 
+                            test_data_paths: Text):
+
+        module_path = importer_config.pop("name", None)
+        if module_path == DrugImporter.__name__:
+            importer_class = DrugImporter
+        else:
+            try:
+                importer_class = drugai.utils.common.class_from_module_path()
+            except (AttributeError, ImportError):
+                logging.warning(f"Importer '{module_path}' not found.")
+                return None
+        importer_config = dict(**importer_config)
+        
